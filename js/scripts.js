11a7c6c9cd3be9eee42a91184c0893b3561e2e14
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-26 18:03:59 -0800

    Add in event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,26 @@
 
 
 //User Interface Logic 
+function hideResults() {
+  document.getElementsByClassName("first").setAttribute("class", "hidden");
+  document.getElementByClassName("last").setAttribute("class", "hidden");
+  document.getElementByClassName("color").setAttribute("class", "hidden");
+  document.getElementByClassName("food").setAttribute("class", "hidden");
+  document.getElementByClassName("anime").setAttribute("class", "hidden");
+}
+
+window.addEventListener("load",function() {
+  hideResults();
+  const favoriteColor = document.getElementByClassName("color").value;
+  const selection1 = document.getElementByClassName("food").value;
+  const selection2 = document.getElementByClassName("anime").value;
+  const firstName = document.getElementClassName("first").value;
+  const lastName = document.getElementByClassName("last").value;
+
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit",function(event) {
+    event.preventDefault();
+    
+  });
+});
