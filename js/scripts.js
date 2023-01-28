fe1ae1e9e073986e968c2715d298ef67a1be270f
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-27 16:20:39 -0800

    Fix problem with branching

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@
 
 
 
-
-// User Interface Logic 
+//User Interface Logic
 function hideResults() {
   let firstName = document.getElementById("inputGroup-sizing-default1").value;
   let lastName = document.getElementById("inputGroup-sizing-default2").value;
@@ -19,31 +18,40 @@ function handleSelect(event) {
 
 window.addEventListener("load",function() {
   hideResults();
-  let firstName = document.getElementById("inputGroup-sizing-default1").value;
-  let lastName = document.getElementById("inputGroup-sizing-default2").value;
-  let favoriteColor = document.getElementById("exampleColorInput").value;
-  let foodChoice = document.getElementById("foodSelect").value;
-  let animeChoice = document.getElementById("animeSelect").value;
-
 
   let form = document.querySelector("form");
   form.addEventListener("submit",function(event) {
     event.preventDefault();
+    let foodChoice = document.getElementById("foodSelect").value;
 
-    document.getElementById("foodSelect").addEventListener("submit", handleSelect);
+    document.getElementById("foodSelect").addEventListener("submit", (event));
     document.getElementById("animeSelect").addEventListener("submit", handleSelect);
 
     document.getElementById("results").removeAttribute("class");
+    document.getElementById("swift").removeAttribute("class");
+    
+    console.log("Correctly giving back right values?=", document.getElementById("foodSelect").value);
 
-    if (results) {
-      if (foodChoice === "Pancakes") {
-        document.getElementById("swift").removeAttribute("class");
-      } else if (foodChoice === "Lasagna") {
-        document.getElementById("go").removeAttribute("class");
-      } else if (foodChoice === "Sushi") {
-        document.getElementById("python").removeAttribute("class");
-      } else if (foodChoice === "Tacos")
-        document.getElementById("ruby").removeAttribute("class");
-       }
+    if (foodChoice === "Pancakes") { 
+      document.getElementById("swift").removeAttribute("class");
+    } else if (foodChoice === "Lasagna") { 
+      document.getElementById("go").removeAttribute("class");
+    } else if (foodChoice === "Sushi") {
+      document.getElementById("python").removeAttribute("class");
+    } else {
+      document.getElementById("ruby").removeAttribute("class");
+    }
+
+      // if (foodChoice === "Pancakes") {
+      //   document.getElementById("swift").removeAttribute("class");
+      // } else if (foodChoice === "Lasagna") {
+      //   document.getElementById("go").removeAttribute("class");
+      // } else if (foodChoice === "Sushi") {
+      //   document.getElementById("python").removeAttribute("class");
+      // } 
+      
+      // else (foodChoice === "Tacos") 
+      //   document.getElementById("ruby").removeAttribute("class");
+      
   });
 });
